91f889852b5518082f46bcbb5540cdafea7c2e5a
ZiF1R <do-alex03@mail.ru>
2025-04-24 21:55:31 +0300

    lexer update: new tokens, tests, keywords

--- src/lexer/Lexer.ts
+++ src/lexer/Lexer.ts
@@ -1,36 +1,53 @@
 import Token, {TokenKind} from "./Token";
 import Keywords from "./Keywords";
 import Types from "./Types";
+import {str_intern} from "./str_intern";
 
-class LexerRegExpCtl {
-    public reg_exp_kind: Record<TokenKind, RegExp> = {
-        [TokenKind.TOKEN_LAMBDA]: /^=>/,
-        [TokenKind.TOKEN_INT]: /^\d+/,
-        [TokenKind.TOKEN_IDENTIFIER]: /^[a-zA-Z_]+/,
-        [TokenKind.TOKEN_EQUAL]: /^~/,
-        [TokenKind.TOKEN_ASSIGN]: /^=/,
-        [TokenKind.TOKEN_PLUS]: /^\+/,
-        [TokenKind.TOKEN_MINUS]: /^-/,
-        [TokenKind.TOKEN_MULTIPLY]: /^\*/,
-        [TokenKind.TOKEN_DIVIDE]: /^\//,
-        [TokenKind.TOKEN_SEMI]: /^;/,
-        [TokenKind.TOKEN_COLON]: /^:/,
-        [TokenKind.TOKEN_WHITESPACE]: /^[\s\n\t\r]+/,
+const TOKEN_REGEX: Record<TokenKind, RegExp> = {
+    [TokenKind.TOKEN_LAMBDA]: /^=>/,
+    [TokenKind.TOKEN_BINARY]: /^0b[01]+(?:\b|$)/,
+    [TokenKind.TOKEN_HEX]: /^0h[0-9A-Fa-f]+(?:\b|$)/,
+    [TokenKind.TOKEN_OCTAL]: /^0o[0-7]+(?:\b|$)/,
+    [TokenKind.TOKEN_FLOAT]: /^[\-+]?\d+\.\d+(?:\b|$)/,
+    [TokenKind.TOKEN_INT]: /^[\-+]?\d+(?:\b|$)/,
+    [TokenKind.TOKEN_IDENTIFIER]: /^[a-zA-Z_]\w*(?=\b|$)/,
+    [TokenKind.TOKEN_EQUAL]: /^==/,
+    [TokenKind.TOKEN_ASSIGN]: /^=(?=[^=])/,
+    [TokenKind.TOKEN_INCREMENT_PREFIX]: /^\+\+[a-zA-Z_]\w*/,
+    [TokenKind.TOKEN_INCREMENT_POSTFIX]: /^[a-zA-Z_]\w*\+\+/,
+    [TokenKind.TOKEN_DECREMENT_PREFIX]: /^--[a-zA-Z_]\w*/,
+    [TokenKind.TOKEN_DECREMENT_POSTFIX]: /^[a-zA-Z_]\w*--/,
+    [TokenKind.TOKEN_PLUS]: /^\+/,
+    [TokenKind.TOKEN_MINUS]: /^-/,
+    [TokenKind.TOKEN_MULTIPLY]: /^\*/,
+    [TokenKind.TOKEN_DIVIDE]: /^\//,
+    [TokenKind.TOKEN_SEMI]: /^;/,
+    [TokenKind.TOKEN_COLON]: /^:/,
+    [TokenKind.TOKEN_LINE_TERMINATOR]: /^[\n\r]+/,
+    [TokenKind.TOKEN_WHITESPACE]: /^[\s\t]+/,
+    [TokenKind.TOKEN_INLINE_COMMENT]: /^#[^-].*/,
+    [TokenKind.TOKEN_BLOCK_COMMENT]: /^#-.*-#/ms,
+    [TokenKind.TOKEN_LPAREN]: /^\(/,
+    [TokenKind.TOKEN_RPAREN]: /^\)/,
+    [TokenKind.TOKEN_LBRACE]: /^\{/,
+    [TokenKind.TOKEN_RBRACE]: /^}/,
+    [TokenKind.TOKEN_LBRACKET]: /^\[/,
+    [TokenKind.TOKEN_RBRACKET]: /^]/,
+    [TokenKind.TOKEN_AND_AND]: /^&&(?=\s|$|\b)/,
+    [TokenKind.TOKEN_OR_OR]: /^\|\|(?=\s|$|\b)/,
 
-        // plug
-        [TokenKind.TOKEN_KEYWORD]: /.^/,
-        [TokenKind.TOKEN_TYPE]: /.^/,
-        [TokenKind.TOKEN_DEFAULT]: /.^/,
-        [TokenKind.TOKEN_EOF]: /.^/,
-    }
+    // plug
+    [TokenKind.TOKEN_KEYWORD]: /.^/,
+    [TokenKind.TOKEN_TYPE]: /.^/,
+    [TokenKind.TOKEN_DEFAULT]: /.^/,
+    [TokenKind.TOKEN_EOF]: /.^/,
+    [TokenKind.__LENGTH__]: /.^/,
 }
 
 export default class Lexer {
     public stream: string
     public position: number = 0;
     public nextPosition: number = 1;
-    public interns: Record<string, string> = {};
-    #regexpctl: LexerRegExpCtl = new LexerRegExpCtl();
 
     constructor(stream: string) {
         this.stream = stream;
@@ -43,18 +60,18 @@ export default class Lexer {
 
         const remaining = this.stream.slice(this.position);
 
-        for (const [kind, regexp] of Object.entries(this.#regexpctl.reg_exp_kind)) {
+        for (const [kind, regexp] of Object.entries(TOKEN_REGEX)) {
             let match = remaining.match(regexp);
+            let kindNum = Number(kind);
 
             if(!match) {
                 continue;
             }
 
-            let literal = this.#str_intern(match[0]);
+            let literal = str_intern(match[0]);
             let startPos = this.position;
             this.#next_position(literal.length);
-
-            switch (kind) {
+            switch (kindNum) {
                 case (TokenKind.TOKEN_WHITESPACE):
                     return this.next_token();
 
@@ -64,14 +81,14 @@ export default class Lexer {
                     } else if (literal in Keywords) {
                         return new Token(TokenKind.TOKEN_KEYWORD, literal, startPos, this.position);
                     }
-                    return new Token(kind as TokenKind, literal, startPos, this.position);
+                    return new Token(kindNum as TokenKind, literal, startPos, this.position);
 
                 default:
-                    return new Token(kind as TokenKind, literal, startPos, this.position);
+                    return new Token(kindNum as TokenKind, literal, startPos, this.position);
             }
         }
 
-        let literal = this.#str_intern(this.stream[this.position]);
+        let literal = str_intern(this.stream[this.position]);
         let startPos = this.position;
         this.#next_position();
 
@@ -81,15 +98,6 @@ export default class Lexer {
     #next_position(n: number = 1) {
         this.position += n;
         this.nextPosition = this.position + 1;
-    }
-
-    #str_intern(str: string): string {
-        if (str in this.interns) {
-            return this.interns[str];
-        } else {
-            this.interns[str] = str;
-            return str;
-        }
     }
 
     static token_cmp(t1: Token, t2: Token): boolean {
